1dcf770e7ab3e9320283d82d3241c9a89963cb67
Jared <matsonj2013@gmail.com>
2025-03-11 22:46:03 -0600

    redirect issues

--- scripts/script.js
+++ scripts/script.js
@@ -24,7 +24,7 @@ function scrollToDiv(location, pageURL = null){
         document.getElementById(location).scrollIntoView({behavior: "smooth", block: "center"});
     } //On correct page
     else{
-        window.location.href = 'index' + '#' + location;
+        window.location.href = '/' + '#' + location;
        // document.getElementById(location).scrollIntoView({behavior: "smooth", block: "center"});
     }
     burgerMenu(true)
